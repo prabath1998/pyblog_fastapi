77381f0b7b5b7d2b4e568a76ac0bf7395f090158
prabath1998 <prabhathudayanga1998@gmail.com>
2023-10-01 18:57:41 +0530

    create db connection

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-import schema
+import schema, models
+from database import engine
 
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
 
 @app.post("/blog")
 def create(request: schema.Blog):
