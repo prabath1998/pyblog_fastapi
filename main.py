402aed90a1479ac9082e271683dfac0a6ffe3102
prabath1998 <prabhathudayanga1998@gmail.com>
2023-10-01 20:57:42 +0530

    add crud operations

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 import schema, models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -15,8 +15,8 @@ def get_db():
     finally:
         db.close()
 
-
-@app.post("/blog")
+# create new blog
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create(request: schema.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -24,8 +24,41 @@ def create(request: schema.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+# delete blog
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    # db.query(models.Blog).filter(models.Blog.id == id).delete()
+    blog = (db.query(models.Blog).filter(models.Blog.id == id))
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    else:
+        blog.delete()
+    db.commit()
+    return {'Deleted'}
 
-@app.get('/blog')
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schema.Blog, db: Session = Depends(get_db)):
+    blog = (db.query(models.Blog).filter(models.Blog.id == id))
+    if not blog.first():
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,detail=f"Blog with id {id} not found")
+    else:
+        blog.update({'title': request.title, 'body': request.body})
+    db.commit()
+    return 'Updated successfully'
+
+
+@app.get('/blog', status_code=200)
 def get_all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
+
+
+@app.get('/get/{id}', status_code=200)
+def show(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} not found!")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f"Blog with the id {id} not found!"}
+    return blog
