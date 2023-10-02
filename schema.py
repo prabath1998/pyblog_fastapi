52188a5bdbaf05e925f5e354b9d12ee5aa6b2a28
prabath1998 <prabhathudayanga1998@gmail.com>
2023-10-02 21:38:30 +0530

    password hashing & change api docs

--- schema.py
+++ schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List
 
 
 class Blog(BaseModel):
@@ -6,9 +7,24 @@ class Blog(BaseModel):
     body: str
 
 
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class show_user(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog] = []
+
+    class Config:
+        orm_mode = True
+
 class show_blog(BaseModel):
     title: str
     body: str
+    creator: show_user
 
-    class Config():
+    class Config:
         orm_mode = True
